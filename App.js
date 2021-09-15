50bced7b74be0af5868064d205805de277324b4e
sid6145 <sidtdesh@gmail.com>
2021-09-15 12:31:49 +0530

    added header

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Chip } from 'react-native-paper';
+import Header from './Header';
 
 export default function App() {
+
+  const [text, setText] = useState('')
+  const [news, setNews] = useState([])
+
+  useEffect(() => {
+
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <ScrollView horizontal={true} style={styles.category}>
+      <Chip style={styles.chip} icon="information" onPress={(e) => console.log(e.target.textContent)}>Technology</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Health</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      <Chip style={styles.chip} icon="information" onPress={() => console.log('Pressed')}>Example Chip</Chip>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +35,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fefae0',
   },
+
+  category:{
+    height: '80px',
+    padding:'20px'
+  },
+
+  chip:{
+    marginRight: '5px'
+  }
 });
